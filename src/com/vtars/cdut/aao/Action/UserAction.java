9e2e82cefc7f0b0222531072947c8f6c5e64afea
Davisz <Davisz@cdc-d-davisz-w7.synnex.org>
2014-12-01 18:12:20 +0800

    继续修改哈

--- src/com/vtars/cdut/aao/Action/UserAction.java
+++ src/com/vtars/cdut/aao/Action/UserAction.java
@@ -74,66 +74,63 @@ public class UserAction extends ActionSupport implements Action,
 	}
 
 	public String userLogin() {
-		// FetchLogin fl = new FetchLogin();
-		// boolean isRight = fl.isLoginRight(username, password);
-		// if (!isRight) {
-		// return INPUT;
-		// // 加个提示 账号密码不匹配
-		// }
-		// String sessionid = fl.getSessionId();
-		// // 将aaosession设置到session里
-		// request.getSession().setAttribute("aaosession", sessionid);
-		String aString = username + password;
-		System.out.println(aString);
-		return SUCCESS;
+		FetchLogin fl = new FetchLogin();
+		boolean isRight = fl.isLoginRight(username, password);
+		if (!isRight) {
+			return INPUT;
+			// 加个提示 账号密码不匹配
+		}
+		String sessionid = fl.getSessionId();
+		// 将aaosession设置到session里
+		request.getSession().setAttribute("aaosession", sessionid);
 
-		/*
-		 * // 这里增加一个验证是否存在未评价的函数 ArrayList<String> al =
-		 * CheckExitsNoEvaluate(lw.getSessionId()); if (al != null && al.size()
-		 * > 1) { System.out.println("未评价处理调试跟踪:" + username);
-		 * request.setAttribute("al", al);
-		 * request.getRequestDispatcher("/Home/waitEvaluateList.jsp").forward(
-		 * request, response); return; } // 查看是否存在该用户 /存在则更新密码 userbean user =
-		 * us.UserExistCheck(username); if (null == user) // 不存在则创建新用户 {
-		 * us.addUser(username, password); logger.info("欢迎新用户：" + username);
-		 * 
-		 * // 以下纯属是为了减少ifesle而增加的 即为了增加通用性 user = new userbean();
-		 * user.setStudyid(username); user.setPassword(password);
-		 * user.setActive(false); }
-		 * 
-		 * // 教务系统数据与网站数据不同 更新 if (!user.getPassword().equals(password.trim()))
-		 * { us.UpdataPassword(username, password);
-		 * logger.info("教务系统数据与网站数据不同!执行更新"); }
-		 * 
-		 * // 走到这里 说明已经登录成功了
-		 * 
-		 * // 这里应该启动一个线程将成绩down下来存到数据库 DownGradesThread dgt = new
-		 * DownGradesThread(aaosession, user); dgt.start();
-		 * 
-		 * request.getSession(true).setAttribute("user", user); Calendar nowtime
-		 * = Calendar.getInstance(); SimpleDateFormat format = new
-		 * SimpleDateFormat("yyyy-MM-dd-HH-mm"); String time =
-		 * format.format(nowtime.getTime());
-		 * request.getSession().setAttribute("logintime", time);
-		 * 
-		 * 
-		 * // 检查用户是否已激活 if (false == user.isActive()) { // N 转发到信息不全界面
-		 * request.setAttribute("user", user);
-		 * request.getRequestDispatcher("/Home/LinkwithEmail.jsp")
-		 * .forward(request, response); return; }
-		 *//**
-		 * 设置多说评论登录cookie 过期时间2天
-		 */
-		/*
-		 * String showname = (user.getNick() == null ||
-		 * "".equals(user.getNick())) ? username : user.getNick(); Cookie
-		 * duoshuoCookie = new Cookie("duoshuo_token", LocalComment.dojwt(
-		 * username, showname)); duoshuoCookie.setMaxAge(60 * 60 * 24 * 2);
-		 * duoshuoCookie.setPath(request.getContextPath());
-		 * response.addCookie(duoshuoCookie);
-		 * response.sendRedirect(request.getContextPath() +
-		 * "/Home/MyAAO/index.jsp");
-		 */
+		
+		// 这里增加一个验证是否存在未评价的函数 ArrayList<String> al =
+		CheckExitsNoEvaluate(lw.getSessionId()); if (al != null && al.size()
+		> 1) { System.out.println("未评价处理调试跟踪:" + username);
+		request.setAttribute("al", al);
+		request.getRequestDispatcher("/Home/waitEvaluateList.jsp").forward(
+		request, response); return; } // 查看是否存在该用户 /存在则更新密码 userbean user =
+		us.UserExistCheck(username); if (null == user) // 不存在则创建新用户 {
+		us.addUser(username, password); logger.info("欢迎新用户：" + username);
+		
+		// 以下纯属是为了减少ifesle而增加的 即为了增加通用性 user = new userbean();
+		user.setStudyid(username); user.setPassword(password);
+		user.setActive(false); }
+		
+		// 教务系统数据与网站数据不同 更新 if (!user.getPassword().equals(password.trim()))
+		{ us.UpdataPassword(username, password);
+		logger.info("教务系统数据与网站数据不同!执行更新"); }
+		
+		// 走到这里 说明已经登录成功了
+		
+		// 这里应该启动一个线程将成绩down下来存到数据库 DownGradesThread dgt = new
+		DownGradesThread(aaosession, user); dgt.start();
+		
+		request.getSession(true).setAttribute("user", user); Calendar nowtime
+		= Calendar.getInstance(); SimpleDateFormat format = new
+		SimpleDateFormat("yyyy-MM-dd-HH-mm"); String time =
+		format.format(nowtime.getTime());
+		request.getSession().setAttribute("logintime", time);
+		
+		
+		// 检查用户是否已激活 if (false == user.isActive()) { // N 转发到信息不全界面
+		request.setAttribute("user", user);
+		request.getRequestDispatcher("/Home/LinkwithEmail.jsp")
+		.forward(request, response); return; }
+		/**
+		设置多说评论登录cookie 过期时间2天
+		
+		
+//		String showname = (user.getNick() == null ||
+//		"".equals(user.getNick())) ? username : user.getNick(); Cookie
+//		duoshuoCookie = new Cookie("duoshuo_token", LocalComment.dojwt(
+//		username, showname)); duoshuoCookie.setMaxAge(60 * 60 * 24 * 2);
+//		duoshuoCookie.setPath(request.getContextPath());
+//		response.addCookie(duoshuoCookie);
+//		response.sendRedirect(request.getContextPath() +
+//		"/Home/MyAAO/index.jsp");
+		
 
 	}
 
